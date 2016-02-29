5f03cd67281ff372f77af0d5551f38f5d55617b8
AndruAlex <andrushoka@gmail.com>
2016-02-29 21:07:01 +0200

    shop

--- www/js/main.js
+++ www/js/main.js
@@ -356,3 +356,37 @@ $(function($){
     });
   })
 });
+$(function() {
+    $( "#slider-range-price" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  });
+
+$(function() {
+    $( "#slider-range-voltage" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  });
+  $(function($){
+  $(function(){
+    $(".mobfilterico").click(function(){
+      $(".mobFilterbar").toggleClass("show");
+    }); 
+  })
+});
